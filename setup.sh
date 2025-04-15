1e36c4fa50ed4e16b3c8faf4693f408b1884d2f7
Ciprian Oprisa <coprisa@bitdefender.com>
2025-04-15 21:54:23 +0300

    added setup and stop scripts

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker.io docker-compose-plugin
+sudo usermod -aG docker $USER
--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
+echo "       ♪ ♫  ♪"
+echo "       ( )"
+echo "      <|===>"
+echo "      / \\"
+echo "     /___\\     <-- The Piper Sensor"
+echo "    |     |"
+echo ""
+echo "          🐛   🦠   🐞   💣   👾   🧟‍♂️"
+
 docker compose up -d
