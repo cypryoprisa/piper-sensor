397656174015a18c0659eb716f54c7516c03e276
Ciprian Oprisa <coprisa@bitdefender.com>
2025-04-16 16:37:22 +0300

    fixed reset-all.sh to not ask for confirmations

--- reset-all.sh
+++ reset-all.sh
@@ -5,5 +5,5 @@ docker volume rm piper-sensor_graylog-datanode \
                  piper-sensor_graylog_data \
                  piper-sensor_mongodb_config \
                  piper-sensor_mongodb_data
-docker container prune
-docker network prune
+docker container prune -f
+docker network prune -f
